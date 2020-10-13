cc3f986041dcfa51c223da84d0264480d21bbb7d
Miguel Pimentel <miguel.silva@tembici.com>
2020-10-13 17:26:18 -0300

    Added tableview and tableviewcell

---
+++ Escola em Casa/Controllers/FAQTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FAQTableViewCell.swift
+//  Escola em Casa
+//
+//  Created by Miguel Pimentel on 13/10/20.
+//  Copyright © 2020 Laércio Silva de Sousa Júnior. All rights reserved.
+//
+
+import UIKit
+
+class FAQTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Escola em Casa/Controllers/FAQViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FAQViewController.swift
+//  Escola em Casa
+//
+//  Created by Miguel Pimentel on 13/10/20.
+//  Copyright © 2020 Laércio Silva de Sousa Júnior. All rights reserved.
+//
+
+import UIKit
+
+class FAQViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
