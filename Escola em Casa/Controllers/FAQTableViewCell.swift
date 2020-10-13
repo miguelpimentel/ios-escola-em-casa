f1762e9194d4dad4638a8f0f87a409bc7c1e9a92
Miguel Pimentel <miguel.silva@tembici.com>
2020-10-13 18:36:07 -0300

    Added expandable logic

--- Escola em Casa/Controllers/FAQTableViewCell.swift
+++ Escola em Casa/Controllers/FAQTableViewCell.swift
@@ -7,18 +7,81 @@
 //
 
 import UIKit
+import SnapKit
+
+protocol FAQTableViewCellDelegate: class {
+    func willLayoutChange(_ cell: FAQTableViewCell)
+}
 
 class FAQTableViewCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    // MARK: - Properties
+
+    var isExpanded: Bool = false {
+        didSet {
+            if !isExpanded {
+                descriptionTextView.snp.makeConstraints { $0.height.equalTo(0) }
+            } else {
+                descriptionTextView.snp.makeConstraints {
+                    $0.width.equalToSuperview().inset(10)
+                    $0.height.greaterThanOrEqualTo(100)
+                }
+            }
+        }
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
+    private lazy var stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.alignment = .fill
+        stackView.spacing = 10
 
-        // Configure the view for the selected state
+        return stackView
+    }()
+
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "O governo pagará pelo uso da plataforma Meet Google?"
+        label.numberOfLines = 2
+        label.textColor = .red
+
+        return label
+    }()
+
+    private lazy var descriptionTextView: UITextView = {
+        let textView = UITextView()
+        textView.text = "Mesmo quando liberado o patrocínio dos dados, não será pago pelo Governo o uso da plataforma Meet Google, pois só é possível utilizá-la através do seu próprio  aplicativo. É responsabilidade do governo arcar somente pelas plataformas que são abertas dentro do nosso próprio aplicativo, Escola em Casa DF."
+
+        return textView
+    }()
+
+    // MARK: - Methods
+
+    func setup() {
+        setupViews()
+        setupConstraints()
     }
 
+    private func setupViews() {
+        addSubview(stackView)
+        stackView.addArrangedSubview(titleLabel)
+        stackView.addArrangedSubview(descriptionTextView)
+    }
+
+    private func setupConstraints() {
+        stackView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+            $0.height.lessThanOrEqualTo(200)
+        }
+
+        titleLabel.snp.makeConstraints {
+            $0.width.equalToSuperview().inset(10)
+            $0.height.greaterThanOrEqualTo(20)
+        }
+
+        descriptionTextView.snp.makeConstraints {
+            $0.width.equalToSuperview()
+            $0.height.equalTo(0)
+        }
+    }
 }
--- Escola em Casa/Controllers/FAQViewController.swift
+++ Escola em Casa/Controllers/FAQViewController.swift
@@ -10,21 +10,65 @@ import UIKit
 
 class FAQViewController: UIViewController {
 
+    // MARK: - Properties
+
+    var content = [1]
+
+    @IBOutlet weak var tableView: UITableView! {
+        didSet {
+            tableView.delegate = self
+            tableView.dataSource = self
+
+            tableView.separatorStyle = .none
+            tableView.tableFooterView = UIView() // Remove empty lines
+            tableView.estimatedRowHeight = 2.0
+            tableView.rowHeight = UITableView.automaticDimension
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
+}
 
-    /*
-    // MARK: - Navigation
+extension FAQViewController: UITableViewDataSource {
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
     }
-    */
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = FAQTableViewCell()
+        cell.setup()
+        cell.isExpanded = false
+
+        return cell
+    }
+}
+
+extension FAQViewController: UITableViewDelegate {
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard let cell = tableView.cellForRow(at: indexPath) as? FAQTableViewCell else { return }
+
+        UIView.animate(withDuration: 0.3, animations: {
+            tableView.beginUpdates()
+            cell.isExpanded = !cell.isExpanded
+            tableView.scrollToRow(at: indexPath, at: UITableView.ScrollPosition.top, animated: true)
+            tableView.reloadData()
+            tableView.endUpdates()
+        })
+    }
+
+    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        guard let cell = tableView.cellForRow(at: indexPath) as? FAQTableViewCell else { return }
+
+        UIView.animate(withDuration: 0.3, animations: {
+            tableView.beginUpdates()
+            cell.isExpanded = false
+            tableView.endUpdates()
+            tableView.reloadData()
+        })
+    }
 }
