c8cea9ee889c260f5fc3b054846fec79fc683d7a
Miguel Pimentel <miguel.silva@tembici.com>
2020-11-02 22:02:23 -0300

    Added minimal changes

--- Escola em Casa/AppDelegate.swift
+++ Escola em Casa/AppDelegate.swift
@@ -4,14 +4,12 @@ import SmiSdkVpn
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
-
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         SmiSdk.initSponsoredVPN("API-KEY", showSDMessage: false)
 
         return true
     }
-
 
     // MARK: UISceneSession Lifecycle
     @available(iOS 13.0, *)
@@ -26,7 +24,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
 }
 
