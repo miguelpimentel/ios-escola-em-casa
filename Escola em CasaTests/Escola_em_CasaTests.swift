7ddf02b55054cfe8b2c01334c6fd7cb9e8943ea6
Miguel Pimentel <miguel.silva@tembici.com>
2020-11-11 17:54:42 -0300

    Added test target

---
+++ Escola em CasaTests/Escola_em_CasaTests.swift
@@ -0,0 +1,33 @@
+//
+//  Escola_em_CasaTests.swift
+//  Escola em CasaTests
+//
+//  Created by Miguel Pimentel on 11/11/20.
+//  Copyright © 2020 Laércio Silva de Sousa Júnior. All rights reserved.
+//
+
+import XCTest
+
+class Escola_em_CasaTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
